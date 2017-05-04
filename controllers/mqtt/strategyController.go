b4d1d1dcaba82dd0be9182c295e27497c88c0792
pluslei <pluslei@foxmail.com>
2017-05-04 15:21:59 +0800

    wechat

--- controllers/mqtt/strategyController.go
+++ controllers/mqtt/strategyController.go
@@ -326,7 +326,7 @@ func parseStrategyMsg(msg string) bool {
 	info.OperType = OPERATE_ADD
 	info.MsgType = MSG_TYPE_STRATEGY_ADD
 	topic := info.Room
-	// sendmsg := info.Data
+	sendmsg := info.Data
 
 	beego.Debug("info", info)
 
@@ -337,7 +337,7 @@ func parseStrategyMsg(msg string) bool {
 	}
 
 	mq.SendMessage(topic, v)
-	// SendWeChatStrategy(topic, sendmsg) // send to wechat
+	SendWeChatStrategy(topic, sendmsg) // send to wechat
 	// 消息入库
 	editStrageydata(info)
 	return true
@@ -366,13 +366,14 @@ func parseEditStrategyMsg(msg string) bool {
 
 		mq.SendMessage(topic, v)
 	*/
-	//SendWeChatStrategy(topic, sendmsg) // send to wechat
+	// SendWeChatStrategy(topic, sendmsg) // send to wechat
 	// 消息入库
 	editStrageydata(info)
 	return true
 }
 
 func SendWeChatStrategy(room, msg string) {
+	beego.Debug("send wechat aaaaaaaaaaaaa")
 	arr, ok := wechat.MapUname[room]
 	if ok {
 		for _, v := range arr {
@@ -488,14 +489,20 @@ func editStrategyContent(info *StrategyInfo) {
 	OPERATETYPE := info.OperType
 	switch OPERATETYPE {
 	case OPERATE_ADD:
-		fileName := haoindex.GetWxServerImg(info.FileName)
 		var strategy m.Strategy
+		if info.FileName == "" {
+			strategy.FileName = ""
+		} else {
+			fileName := haoindex.GetWxServerImg(info.FileName)
+			strategy.FileName = fileName
+		}
+
 		strategy.Room = info.Room
 		strategy.Icon = info.Icon
 		strategy.Name = info.Name
 		strategy.Titel = info.Titel
 		strategy.Data = info.Data
-		strategy.FileName = fileName
+
 		strategy.TxtColour = info.TxtColour
 		strategy.IsTop = info.IsTop
 		strategy.IsDelete = info.IsDelete
@@ -521,7 +528,7 @@ func editStrategyContent(info *StrategyInfo) {
 		strategy.Name = info.Name
 		strategy.Titel = info.Titel
 		strategy.Data = info.Data
-		if strategyInfo.WxServerImgid != info.FileName {
+		if strategyInfo.WxServerImgid != info.FileName && info.FileName != "" {
 			fileName := haoindex.GetWxServerImg(info.FileName)
 			strategy.FileName = fileName
 			strategy.WxServerImgid = info.FileName
