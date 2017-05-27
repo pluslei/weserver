9867eb640493ae36d5f84cd74ab9b08f40942fd3
xiaxufeng <864193906@qq.com>
2017-05-27 18:33:26 +0800

    'cookie'

--- controllers/haoindex/indexController.go
+++ controllers/haoindex/indexController.go
@@ -84,6 +84,7 @@ func (this *IndexController) LoginHandle() {
 	var user = new(m.User)
 	username := this.GetString("username")
 	password := this.GetString("password")
+	beego.Debug("username password", username, password)
 	if len(username) <= 0 || len(password) <= 0 {
 		this.AlertBack("请填写账户信息")
 		return
@@ -100,9 +101,10 @@ func (this *IndexController) LoginHandle() {
 		beego.Debug("PassWord Error")
 		return
 	}
+	
 
 	this.SetSession("LoginInfo", user)
-	this.Redirect("/chooseloginmode", 302)
+	// this.Redirect("/chooseloginmode", 302)
 }
 
 func (this *IndexController) GetLoginMode() {
